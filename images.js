75a3dcaa82b07c661baefe54c507650274be63c6
Denis THEVENOT <76215323+DenisTHEVENOT@users.noreply.github.com>
2025-07-15 16:38:04 +0200

    Add files via upload

---
+++ images.js
@@ -0,0 +1,878 @@
+const rebus = [
+    {
+        "img": "âge.jpg",
+        "tags": "age,âge"
+    },
+    {
+        "img": "ail.jpg",
+        "tags": "ail,aille,aye"
+    },
+    {
+        "img": "aile.jpg",
+        "tags": "aile,elle"
+    },
+    {
+        "img": "air.jpg",
+        "tags": "air,er,aire,ère,erre"
+    },
+    {
+        "img": "allée.jpg",
+        "tags": "alé,allée,allé,aller"
+    },
+    {
+        "img": "âme.jpg",
+        "tags": "âme,ame,am,amm,ham"
+    },
+    {
+        "img": "an.jpg",
+        "tags": "an,en,ant,ent,anx,ans"
+    },
+    {
+        "img": "âne.jpg",
+        "tags": "ane,âne,anne"
+    },
+    {
+        "img": "anse.jpg",
+        "tags": "anse,ance,ence,ense"
+    },
+    {
+        "img": "arche.jpg",
+        "tags": "arche"
+    },
+    {
+        "img": "arête.jpg",
+        "tags": "arrête,arrete,arete,arête,arète"
+    },
+    {
+        "img": "argent.jpg",
+        "tags": "argent,argeant,argen"
+    },
+    {
+        "img": "art.jpg",
+        "tags": "ar,art"
+    },
+    {
+        "img": "at.jpg",
+        "tags": "at,atte,@"
+    },
+    {
+        "img": "atome.jpg",
+        "tags": "atome"
+    },
+    {
+        "img": "avé.jpg",
+        "tags": "avé,ave,avait,avai"
+    },
+    {
+        "img": "aveugle.jpg",
+        "tags": "aveugle,aveugles"
+    },
+    {
+        "img": "avion.jpg",
+        "tags": "avion,avions"
+    },
+    {
+        "img": "beau.jpg",
+        "tags": "beau,bo,bau"
+    },
+    {
+        "img": "beurre.jpg",
+        "tags": "beurre,beure,beur"
+    },
+    {
+        "img": "bite.jpg",
+        "tags": "bite"
+    },
+    {
+        "img": "bon.jpg",
+        "tags": "bon,bond,bont"
+    },
+    {
+        "img": "bonne.jpg",
+        "tags": "bonne,bone"
+    },
+    {
+        "img": "borgne.jpg",
+        "tags": "borgne,borgnes"
+    },
+    {
+        "img": "bout.jpg",
+        "tags": "bout,boue,bou"
+    },
+    {
+        "img": "bulle.jpg",
+        "tags": "bulle,bule"
+    },
+    {
+        "img": "cercle.jpg",
+        "tags": "cercle"
+    },
+    {
+        "img": "chai.jpg",
+        "tags": "chai,chais,chez,ché,chè,chait"
+    },
+    {
+        "img": "champ.jpg",
+        "tags": "champ,chant,chan,chen"
+    },
+    {
+        "img": "char.jpg",
+        "tags": "char,charre,chare"
+    },
+    {
+        "img": "chat.jpg",
+        "tags": "chat,cha,chats,chas"
+    },
+    {
+        "img": "chaud.jpg",
+        "tags": "chaud,cho"
+    },
+    {
+        "img": "chie.jpg",
+        "tags": "chie,chi"
+    },
+    {
+        "img": "chou.jpg",
+        "tags": "chou"
+    },
+    {
+        "img": "chute.jpg",
+        "tags": "chute"
+    },
+    {
+        "img": "cire.jpg",
+        "tags": "cire,sir"
+    },
+    {
+        "img": "clé.jpg",
+        "tags": "clé,clè,cle,clai,clef"
+    },
+    {
+        "img": "coeur.jpg",
+        "tags": "coeur,coeurs,queur,cœur,cœurs"
+    },
+    {
+        "img": "cola.jpg",
+        "tags": "cola"
+    },
+    {
+        "img": "colle.jpg",
+        "tags": "colle,cole,col"
+    },
+    {
+        "img": "con.jpg",
+        "tags": "con"
+    },
+    {
+        "img": "coq.jpg",
+        "tags": "coq,coque,coc,coke,cok,quoc"
+    },
+    {
+        "img": "cou.jpg",
+        "tags": "cou,coup"
+    },
+    {
+        "img": "cri.jpg",
+        "tags": "cri"
+    },
+    {
+        "img": "cul.jpg",
+        "tags": "cul,cu"
+    },
+    {
+        "img": "daim.jpg",
+        "tags": "daim,din"
+    },
+    {
+        "img": "dame.jpg",
+        "tags": "dame,damme"
+    },
+    {
+        "img": "danse.jpg",
+        "tags": "danse,dense"
+    },
+    {
+        "img": "dé.jpg",
+        "tags": "dé,des,dés"
+    },
+    {
+        "img": "dent.jpg",
+        "tags": "dent,dents,dans,den,dan"
+    },
+    {
+        "img": "deux.jpg",
+        "tags": "deux,2,de"
+    },
+    {
+        "img": "dieu.jpg",
+        "tags": "dieu"
+    },
+    {
+        "img": "dix.jpg",
+        "tags": "dix,di,disse,10"
+    },
+    {
+        "img": "dos.jpg",
+        "tags": "dos,do,dau,deau"
+    },
+    {
+        "img": "dû.jpg",
+        "tags": "du,dû"
+    },
+    {
+        "img": "dune.jpg",
+        "tags": "dune"
+    },
+    {
+        "img": "dur.jpg",
+        "tags": "dur,dure"
+    },
+    {
+        "img": "eau.jpg",
+        "tags": "eau,o,au"
+    },
+    {
+        "img": "est.jpg",
+        "tags": "est,este"
+    },
+    {
+        "img": "étai.jpg",
+        "tags": "été,etai,étai,était"
+    },
+    {
+        "img": "et.jpg",
+        "tags": "iti,e.t.,e.t"
+    },
+    {
+        "img": "faim.jpg",
+        "tags": "faim,fin"
+    },
+    {
+        "img": "fa.jpg",
+        "tags": "fa,pha"
+    },
+    {
+        "img": "faim.jpg",
+        "tags": "faim,fin"
+    },
+    {
+        "img": "fan.jpg",
+        "tags": "fan,phan,fen"
+    },
+    {
+        "img": "faux.jpg",
+        "tags": "faux,faut,fo,pho,phô"
+    },
+    {
+        "img": "fée.jpg",
+        "tags": "fée,fé,phé"
+    },
+    {
+        "img": "fer.jpg",
+        "tags": "fer,ferre,faire,fère,fair"
+    },
+    {
+        "img": "ferme.jpg",
+        "tags": "ferme"
+    },
+    {
+        "img": "fête.jpg",
+        "tags": "fête,fete,faite"
+    },
+    {
+        "img": "fille.jpg",
+        "tags": "fille"
+    },
+    {
+        "img": "fils.jpg",
+        "tags": "fils,fisse,phisse,phice,fice"
+    },
+    {
+        "img": "foie.jpg",
+        "tags": "foie,fois,foix,foi"
+    },
+    {
+        "img": "four.jpg",
+        "tags": "four,fourre,foure,fours"
+    },
+    {
+        "img": "gai.jpg",
+        "tags": "gai,guet,gué,guè"
+    },
+    {
+        "img": "gardien.jpg",
+        "tags": "gardien"
+    },
+    {
+        "img": "gare.jpg",
+        "tags": "gare,gar"
+    },
+    {
+        "img": "gars.jpg",
+        "tags": "gars,ga"
+    },
+    {
+        "img": "gorge.jpg",
+        "tags": "gorge,gorges"
+    },
+    {
+        "img": "grand.jpg",
+        "tags": "grand,gran,gren"
+    },
+    {
+        "img": "gros.jpg",
+        "tags": "gros,gro,grau"
+    },
+    {
+        "img": "gueule.jpg",
+        "tags": "gueule"
+    },
+    {
+        "img": "guerre.jpg",
+        "tags": "guerre,guere"
+    },
+    {
+        "img": "haie.jpg",
+        "tags": "haie,est,et,é,è,ê"
+    },
+    {
+        "img": "halles.jpg",
+        "tags": "halles,al,hal,halle,ale,alle"
+    },
+    {
+        "img": "heaume.jpg",
+        "tags": "heaume,ome,home,homme,hôme,ôme"
+    },
+    {
+        "img": "heure.jpg",
+        "tags": "heure,eur"
+    },
+    {
+        "img": "hiver.jpg",
+        "tags": "hiver,iver,ivere,ivère"
+    },
+    {
+        "img": "hotte.jpg",
+        "tags": "hotte,ot,ote"
+    },
+    {
+        "img": "ile.jpg",
+        "tags": "ile,il,île"
+    },
+    {
+        "img": "ion.jpg",
+        "tags": "ion,yon"
+    },
+    {
+        "img": "jambes.jpg",
+        "tags": "jambes,jambe"
+    },
+    {
+        "img": "jet.jpg",
+        "tags": "jet,gé,gè,jai"
+    },
+    {
+        "img": "jeux.jpg",
+        "tags": "jeux,jeu,je"
+    },
+    {
+        "img": "la.jpg",
+        "tags": "la"
+    },
+    {
+        "img": "lait.jpg",
+        "tags": "lait,lai,lé,lè,let,les"
+    },
+    {
+        "img": "lampe.jpg",
+        "tags": "lampe,lemp,lamp,lempe"
+    },
+    {
+        "img": "lance.jpg",
+        "tags": "lance,lanse,lense,lence"
+    },
+    {
+        "img": "langue.jpg",
+        "tags": "langue,lang,leng,lengue"
+    },
+    {
+        "img": "lave.jpg",
+        "tags": "lave"
+    },
+    {
+        "img": "le.jpg",
+        "tags": "le,leu"
+    },
+    {
+        "img": "lettre.jpg",
+        "tags": "lettre,lètre,letre,letr,létre,laitre"
+    },
+    {
+        "img": "ligne.jpg",
+        "tags": "ligne"
+    },
+    {
+        "img": "lit.jpg",
+        "tags": "lit,li"
+    },
+    {
+        "img": "loup.jpg",
+        "tags": "loup,lou"
+    },
+    {
+        "img": "lys.jpg",
+        "tags": "lys,lisse,lice"
+    },
+    {
+        "img": "mai.jpg",
+        "tags": "mai,mais,mé,mè,mê,met,mes"
+    },
+    {
+        "img": "marche.jpg",
+        "tags": "marche"
+    },
+    {
+        "img": "mât.jpg",
+        "tags": "mât,mat,ma"
+    },
+    {
+        "img": "mère.jpg",
+        "tags": "mère,mer,mere"
+    },
+    {
+        "img": "meuh.jpg",
+        "tags": "meuh,me,meu"
+    },
+    {
+        "img": "mie.jpg",
+        "tags": "mie,mi,mit"
+    },
+    {
+        "img": "moi.jpg",
+        "tags": "moi,mois"
+    },
+    {
+        "img": "mont.jpg",
+        "tags": "mont,mon"
+    },
+    {
+        "img": "mort.jpg",
+        "tags": "mort,mord,mor"
+    },
+    {
+        "img": "moulin.jpg",
+        "tags": "moulin"
+    },
+    {
+        "img": "musée.jpg",
+        "tags": "musée,musé,muzée"
+    },
+    {
+        "img": "nasse.jpg",
+        "tags": "nasse,nas,nace"
+    },
+    {
+        "img": "nez.jpg",
+        "tags": "nez,né,nait,nai"
+    },
+    {
+        "img": "nid.jpg",
+        "tags": "nid,ni,nis,nids,nit,nie"
+    },
+    {
+        "img": "noel.jpg",
+        "tags": "noel,noël,noelle"
+    },
+    {
+        "img": "noeud.jpg",
+        "tags": "nœud,noeud,neu,ne"
+    },
+    {
+        "img": "nord.jpg",
+        "tags": "nord,nor,nore"
+    },
+    {
+        "img": "nu.jpg",
+        "tags": "nu,nue"
+    },
+    {
+        "img": "objectif.jpg",
+        "tags": "objectif"
+    },
+    {
+        "img": "oeil.jpg",
+        "tags": "oeil,œil"
+    },
+    {
+        "img": "oeufs.jpg",
+        "tags": "oeufs,œufs,eux,eu,e"
+    },
+    {
+        "img": "oie.jpg",
+        "tags": "oie,oi,oua"
+    },
+    {
+        "img": "ombre.jpg",
+        "tags": "ombre"
+    },
+    {
+        "img": "or.jpg",
+        "tags": "or,hors,ore"
+    },
+    {
+        "img": "oreille.jpg",
+        "tags": "oreille,oreil"
+    },
+    {
+        "img": "ouest.jpg",
+        "tags": "ouest"
+    },
+    {
+        "img": "ours.jpg",
+        "tags": "ours,ourse"
+    },
+    {
+        "img": "part.jpg",
+        "tags": "part,par,pare"
+    },
+    {
+        "img": "pas.jpg",
+        "tags": "pas,pa"
+    },
+    {
+        "img": "père.jpg",
+        "tags": "père,per,pere,pair,paire"
+    },
+    {
+        "img": "pet.jpg",
+        "tags": "pet,pèt,pè,pé,pê,pai,paix"
+    },
+    {
+        "img": "petit.jpg",
+        "tags": "petit,peti"
+    },
+    {
+        "img": "peur.jpg",
+        "tags": "peur"
+    },
+    {
+        "img": "phare.jpg",
+        "tags": "phare,far,fare,phar"
+    },
+    {
+        "img": "pie.jpg",
+        "tags": "pie,pi,pis"
+    },
+    {
+        "img": "pied.jpg",
+        "tags": "pied,pié,pieds,piés"
+    },
+    {
+        "img": "piles.jpg",
+        "tags": "piles,pile,pil"
+    },
+    {
+        "img": "planète.jpg",
+        "tags": "planète,planete"
+    },
+    {
+        "img": "pluie.jpg",
+        "tags": "pluie,plui"
+    },
+    {
+        "img": "poire.jpg",
+        "tags": "poire,poir,poar"
+    },
+    {
+        "img": "poids.jpg",
+        "tags": "poids,poi,pois,poa"
+    },
+    {
+        "img": "pompe.jpg",
+        "tags": "pompe,pompes,pomp"
+    },
+    {
+        "img": "port.jpg",
+        "tags": "port,porc,pore,paur,peaur"
+    },
+    {
+        "img": "porte.jpg",
+        "tags": "porte"
+    },
+    {
+        "img": "pot.jpg",
+        "tags": "pot,po,peau,pau"
+    },
+    {
+        "img": "poule.jpg",
+        "tags": "poule,poulle,poul"
+    },
+    {
+        "img": "pré.jpg",
+        "tags": "pré,pre,pret,prêt,prè"
+    },
+    {
+        "img": "prendre.jpg",
+        "tags": "prendre,prandre"
+    },
+    {
+        "img": "presse.jpg",
+        "tags": "presse"
+    },
+    {
+        "img": "puce.jpg",
+        "tags": "puce,pusse,pus"
+    },
+    {
+        "img": "pue.jpg",
+        "tags": "pue,pu"
+    },
+    {
+        "img": "quai.jpg",
+        "tags": "quai,qué,què,quais,quet,quets,quêt"
+    },
+    {
+        "img": "queue.jpg",
+        "tags": "queue,que"
+    },
+    {
+        "img": "qui.jpg",
+        "tags": "qui"
+    },
+    {
+        "img": "raie.jpg",
+        "tags": "raie,ré,rè,rai"
+    },
+    {
+        "img": "rat.jpg",
+        "tags": "rat,ra"
+    },
+    {
+        "img": "riz.jpg",
+        "tags": "riz,ri,rit"
+    },
+    {
+        "img": "roi.jpg",
+        "tags": "roi,roa,rôa,roie,rois,roies"
+    },
+    {
+        "img": "rose.jpg",
+        "tags": "rose"
+    },
+    {
+        "img": "rot.jpg",
+        "tags": "rot,rôt,ro,rô"
+    },
+    {
+        "img": "rue.jpg",
+        "tags": "rue,ru"
+    },
+    {
+        "img": "sac.jpg",
+        "tags": "sac,sacs,saque"
+    },
+    {
+        "img": "sang.jpg",
+        "tags": "sang,sans,san,sen,sent"
+    },
+    {
+        "img": "saigne.jpg",
+        "tags": "saigne,saigner"
+    },
+    {
+        "img": "saint.jpg",
+        "tags": "saint,sein,sin,scin,sain"
+    },
+    {
+        "img": "scie.jpg",
+        "tags": "scie,si,ci"
+    },
+    {
+        "img": "seau.jpg",
+        "tags": "seau,saut,sot,so"
+    },
+    {
+        "img": "selle.jpg",
+        "tags": "selle,sel,sèle"
+    },
+    {
+        "img": "serre.jpg",
+        "tags": "serre,sert,ser"
+    },
+    {
+        "img": "sol.jpg",
+        "tags": "sol,sole,solle"
+    },
+    {
+        "img": "son.jpg",
+        "tags": "son,sont,çon"
+    },
+    {
+        "img": "sonne.jpg",
+        "tags": "sonne,sone"
+    },
+    {
+        "img": "soupe.jpg",
+        "tags": "soupe"
+    },
+    {
+        "img": "sous.jpg",
+        "tags": "sous,sou"
+    },
+    {
+        "img": "stèle.jpg",
+        "tags": "stèle,stelle"
+    },
+    {
+        "img": "sud.jpg",
+        "tags": "sud"
+    },
+    {
+        "img": "sue.jpg",
+        "tags": "sue,su"
+    },
+    {
+        "img": "suie.jpg",
+        "tags": "suie,sui,suis"
+    },
+    {
+        "img": "sur.jpg",
+        "tags": "sur,sure,sûr"
+    },
+    {
+        "img": "taie.jpg",
+        "tags": "taie,tè,té,tai,tê"
+    },
+    {
+        "img": "tas.jpg",
+        "tags": "tas,ta"
+    },
+    {
+        "img": "tee.jpg",
+        "tags": "tee,ti"
+    },
+    {
+        "img": "teint-teindre.jpg",
+        "tags": "teint,teindre,tin,tain,tindre,tein"
+    },
+    {
+        "img": "temps.jpg",
+        "tags": "temps,ten,tan,tant"
+    },
+    {
+        "img": "terre.jpg",
+        "tags": "terre,ter,tère"
+    },
+    {
+        "img": "tête.jpg",
+        "tags": "tête,tete,tet,taite"
+    },
+    {
+        "img": "thé.jpg",
+        "tags": "thé,té"
+    },
+    {
+        "img": "thon.jpg",
+        "tags": "thon,ton"
+    },
+    {
+        "img": "toile.jpg",
+        "tags": "toile,toil"
+    },
+    {
+        "img": "tôt.jpg",
+        "tags": "tôt,tot,to,tau,teau"
+    },
+    {
+        "img": "tour.jpg",
+        "tags": "tour,toure,tours"
+    },
+    {
+        "img": "trait.jpg",
+        "tags": "trait,tré,trè,très,trai"
+    },
+    {
+        "img": "tronc.jpg",
+        "tags": "tronc,tron"
+    },
+    {
+        "img": "trot.jpg",
+        "tags": "trot,trop,tro,trau,treau"
+    },
+    {
+        "img": "trousse.jpg",
+        "tags": "trousse"
+    },
+    {
+        "img": "tue.jpg",
+        "tags": "tue,tu"
+    },
+    {
+        "img": "u.jpg",
+        "tags": "u,eu"
+    },
+    {
+        "img": "un.jpg",
+        "tags": "un,1,ain,in"
+    },
+    {
+        "img": "une.jpg",
+        "tags": "une"
+    },
+    {
+        "img": "vapeur.jpg",
+        "tags": "vapeur"
+    },
+    {
+        "img": "vase.jpg",
+        "tags": "vase"
+    },
+    {
+        "img": "veau.jpg",
+        "tags": "veau,vo,vau"
+    },
+    {
+        "img": "vent.jpg",
+        "tags": "vent,vant,van,ven"
+    },
+    {
+        "img": "vendre.jpg",
+        "tags": "vendre,vandre"
+    },
+    {
+        "img": "ver.jpg",
+        "tags": "ver,verre,vert,vers,vère"
+    },
+    {
+        "img": "vie.jpg",
+        "tags": "vie,vi,vit"
+    },
+    {
+        "img": "vin.jpg",
+        "tags": "vin,vingt,vain"
+    },
+    {
+        "img": "voeu.jpg",
+        "tags": "voeu,veu,ve"
+    },
+    {
+        "img": "voile.jpg",
+        "tags": "voile,voil"
+    },
+    {
+        "img": "voir.jpg",
+        "tags": "voir,voire,voar"
+    },
+    {
+        "img": "vol.jpg",
+        "tags": "vol,vole"
+    },
+    {
+        "img": "zeste.jpg",
+        "tags": "zeste,zest,siste,ziste,sist"
+    },
+    {
+        "img": "zoo.jpg",
+        "tags": "zoo,zo"
+    }
+]
---
+++ script.js
@@ -0,0 +1,293 @@
+const version = `1.11.5.${rebus.length}` // Version du script, avec le nombre d'images
+const sImg = 250
+const angle = 5 // Angle max de rotation aléatoire pour les images
+const wPopup = 1700 // Largeur de la popup
+const hPopup = 1000 // Hauteur de la popup
+let popup = null
+let tagsFiltred = rebus
+let vrac = true // Par défaut, on affiche les cartes en vrac
+let son = true // Par défaut, on joue les sons
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const input = document.getElementById('phrase')
+    const btnGo = document.getElementById('go')
+    const btnReset = document.getElementById('reset')
+    const btnOpen = document.getElementById('open')
+    const btnMenu = document.getElementById('menu')
+    const resultat = document.getElementById('resultat')
+    const filter = document.getElementById('filter')
+    const btnClear = document.getElementById('clear')
+    const inputVrac = document.getElementById('vrac')
+    const inputSon = document.getElementById('son')
+    const inputShowTags = document.getElementById('showtags')
+    const inputHidePhrase = document.getElementById('hidephrase')
+
+    // Chargement des sons
+    const sons = [
+      new Audio('sons/carte01.mp3'),
+      new Audio('sons/carte02.mp3'),
+      new Audio('sons/carte03.mp3'),
+    ]
+    sons.forEach(audio => { audio.load() })
+    const efface = new Audio('sons/efface.mp3')
+    efface.load()
+
+    function genRebus(phrase) {
+        const phonemes = phrase.toLowerCase().split(' ')
+        let images = []
+        resultat.innerHTML = '' // Vide le conteneur avant de le remplir
+
+        if (popup) {
+            popup.document.getElementById('resultat').innerHTML = '' // Vide le contenu de la popup
+        }
+
+        // ajoute les images/lettres pour chaque phonème
+        phonemes.forEach(phoneme => {
+            if (phoneme.includes('\'') && phoneme.length > 2) {
+                const [mot1, mot2] = phoneme.split('\'')
+                if (mot1.length === 1) {
+                    images.push(`${mot1}'`) // Ajoute le premier phonème avec l'apostrophe après
+                    images.push(getImage(mot2)) // Ajoute le second phonème
+                } else {
+                    images.push(getImage(mot1)) // Ajoute le premier phonème
+                    images.push(`'${mot2}`) // Ajoute le second phonème avec l'apostrophe avant
+                }
+            } else if (phoneme.length === 1 || (phoneme.length === 2 && phoneme.includes('\''))) {
+                images.push(phoneme) // Ajoute le phonème tel quel
+            } else {
+                images.push(getImage(phoneme))
+            }
+
+        })
+
+
+        // Si le tableau images n'est pas vide, on affiche le rébus images par images
+        if (images.length) {
+            // Calcul dynamique de la largeur
+            let nbr = [5, 9, 10, 13, 14, 15].includes(images.length) ? 5 : 4
+            if (images.length > 15) nbr = 6
+            resultat.style.width = (nbr * sImg) + 'px'
+            if (popup) {
+                popup.document.getElementById('resultat').style.width = (nbr * sImg) + 'px'
+            }
+
+            // Ajout progressif des images avec délai
+            let i = 0
+            function addNextImage() {
+                const span = creatSpan(images[i])
+
+                addimage(span, resultat)
+                if (popup) {
+                    addimage(span.cloneNode(true), popup.document.getElementById('resultat'))
+                }
+                i++
+                if (i < images.length) {
+                    setTimeout(addNextImage, 300)
+                }
+            }
+
+            addNextImage()
+        } else {
+            resultat.innerHTML = '<p>Aucun rébus trouvé pour cette phrase.</p>'
+        }
+    }
+
+    function getImage(phoneme) {
+        if (!phoneme.includes('\'') || phoneme.length > 1) {
+            // Recherche dans le tableau rebus
+            const found = rebus.find(item => {
+                return item.tags.split(',').includes(phoneme)
+            })
+
+            if (found) {
+                return found.img
+            }
+
+            return `_${phoneme}` // Si pas trouvé, retourne un phonème préfixé par _
+        }
+    }
+
+    function creatSpan(image) {
+        const num = Math.floor(Math.random() * (angle * 2 + 1)) - angle // Nombre aléatoire entre -5 et +5
+        const rot = vrac ? ` rot${num}` : ''
+        const span = document.createElement('span')
+
+        if (image.startsWith('_')) {
+            span.className = `noimg${rot}`
+            span.append('?')
+            const small = document.createElement('small')
+            small.textContent = image.replace(/_/g, '')
+            span.append(small)
+        } else if (image.endsWith('.jpg')) {
+            span.className = `img${rot}`
+            const img = document.createElement('img')
+            img.src = `images/${image}`
+            const small = document.createElement('small')
+            small.textContent = image.replace('.jpg', '')
+            span.append(img)
+            span.append(small)
+        } else {
+            span.className = `lettre${rot}`
+            span.append(image.toUpperCase())
+        }
+
+        return span
+    }
+
+    function addimage(span, parent) {
+        if (son) {
+            const num = Math.floor(Math.random() * 3)
+            sons[num].currentTime = 0 // Réinitialise le son
+            sons[num].play() // Joue le son pour chaque image affichée
+        }
+        parent.append(span)
+        setTimeout(() => {
+            span.classList.add('show')
+        }, 50)
+    }
+
+    function updateTags() {
+        const tagsContainer = document.getElementById('tags')
+        tagsContainer.innerHTML = '' // Vide le conteneur avant de le remplir
+        tagsFiltred.forEach(item => {
+            const tagSpan = document.createElement('span')
+            tagSpan.className = 'tag'
+            tagSpan.textContent = item.tags
+
+            tagSpan.addEventListener('click', () => {
+                const tag = item.tags.split(',')[0] // Prend le premier tag
+                input.value += tag + ' ' // Met à jour l'input avec le tag cliqué
+                const image = getImage(tag)
+                const span = creatSpan(image)
+                addimage(span, resultat)
+            })
+
+            tagsContainer.appendChild(tagSpan)
+        })
+    }
+
+    input.addEventListener('keypress', event => {
+        if (event.key === 'Enter') {
+            event.preventDefault() // Empêche le comportement par défaut de l'input
+            genRebus(event.target.value)
+        }
+    })
+
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+            btnClear.click() // Simule le clic sur le bouton de nettoyage
+            document.querySelector('aside').classList.remove('opened')
+        }
+    })
+
+    btnGo.addEventListener('click', () => {
+        if (input.value.trim()) {
+            genRebus(input.value)
+        }
+    })
+
+    btnReset.addEventListener('click', () => {
+        if (son && input.value.trim() !== '') {
+            efface.currentTime = 0 // Réinitialise le son de distribution
+            efface.play() // Joue le son de distribution
+        }
+
+        input.value = ''
+        resultat.innerHTML = ''
+        input.focus()
+
+        if (popup) {
+            // efface le contenu de la popup
+            popup.document.getElementById('resultat').innerHTML = ''
+
+        }
+    })
+
+    btnMenu.addEventListener('click', () => {
+        document.querySelector('aside').classList.toggle('opened')
+        if (document.querySelector('aside').classList.contains('opened')) {
+            filter.focus()
+        } else {
+            input.focus()
+        }
+    })
+
+    btnOpen.addEventListener('click', () => {
+        if (popup && !popup.closed) {
+            popup.focus()
+            return
+        }
+
+        // calcul la position 'top' pour centrer en hauteur la popup
+        // 73 pour la hauteur de la barre de titre, url et des bordures
+        const top = Math.round((window.screen.height - (hPopup + 73)) / 2)
+
+        popup = window.open(
+          'popup.html',
+          '',
+          `width=1700,height=1000,top=${top},left=110`
+        )
+
+        if (popup) {
+            console.dir(popup)
+            setTimeout(() => {
+                popup.document.querySelector('footer code').textContent = version
+            }, 100)
+            // popup.document.close()
+            // Quand la popup est fermée, remettre popup à null
+            popup.onbeforeunload = () => { popup = null }
+        }
+    })
+
+    btnClear.addEventListener('click', () => {
+        filter.value = ''
+        tagsFiltred = rebus
+        filter.focus()
+        updateTags()
+    })
+
+    filter.addEventListener('input', () => {
+        const searchTerm = filter.value.toLowerCase().trim()
+        if (searchTerm === '') {
+            tagsFiltred = rebus
+        } else {
+            tagsFiltred = rebus.filter(item => {
+                return item.tags.toLowerCase().includes(searchTerm)
+            })
+        }
+
+        // Met à jour les tags affichés
+        updateTags()
+    })
+
+    inputVrac.addEventListener('change', () => {
+        vrac = inputVrac.checked
+    })
+
+    inputSon.addEventListener('change', () => {
+        son = inputSon.checked
+    })
+
+    inputShowTags.addEventListener('change', () => {
+        resultat.classList.toggle('show-tags', inputShowTags.checked)
+    })
+
+    inputHidePhrase.addEventListener('change', () => {
+        document.querySelector('.field').classList.toggle('hide', inputHidePhrase.checked)
+    })
+
+    // actions au démarrage
+    input.focus()
+
+    if (vrac) {
+        inputVrac.checked = true
+    }
+
+    if (son) {
+        inputSon.checked = true
+    }
+
+    updateTags() // Initialiser les tags affichés
+
+    document.querySelector('footer code').textContent = version
+})
